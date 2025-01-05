65eb118f7c8cd17b6c6d4759b04b3934a0474619
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-05 10:45:17 +0530

    create and update js file for counting function

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let stopBtn = document.getElementById('stopBtn');
 let startBtn = document.getElementById('startBtn');
 let resetBtn = document.getElementById('resetBtn');
 
-let msec = 00;
-let secs = 00;
-let mins = 00;
+let msec = 0;
+let secs = 0;
+let mins = 0;
 
 let timerId = null;
 
@@ -16,16 +16,36 @@ startBtn.addEventListener('click', function(){
     timerId = setInterval(startTimer, 10);
 });
 
+stopBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+  
+});
+
+resetBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+    timerDisplay.innerHTML = '00 : 00 : 00';
+    msec = secs = mins = 0; 
+  
+});
+
 function startTimer(){
     msec++;
-    if(msec == 100){
+    if(msec === 100){
         msec = 0;
         secs++;
-        if(secs == 60){
+        if(secs === 60){
             secs = 0;
             mins++;
         }
 
     }
 
-};
+    let msecString = msec < 10 ? `0${msec}` : msec;
+    let secsString = secs < 10 ? `0${secs}` : secs;
+    let minString = mins < 10 ? `0${mins}` : mins;
+
+    timerDisplay.innerHTML = `${minString} : ${secsString} : ${msecString}`;
+    
+
+
+ };
