22271061924392fc08f70518d5003088e43666b0
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-05 10:16:13 +0530

    update js code to buttons

---
+++ script.js
@@ -0,0 +1,31 @@
+let timerDisplay = document.querySelector('.timerDisplay');
+let stopBtn = document.getElementById('stopBtn');
+let startBtn = document.getElementById('startBtn');
+let resetBtn = document.getElementById('resetBtn');
+
+let msec = 00;
+let secs = 00;
+let mins = 00;
+
+let timerId = null;
+
+startBtn.addEventListener('click', function(){
+    if(timerId !== null){
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+});
+
+function startTimer(){
+    msec++;
+    if(msec == 100){
+        msec = 0;
+        secs++;
+        if(secs == 60){
+            secs = 0;
+            mins++;
+        }
+
+    }
+
+};
